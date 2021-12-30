3f3800de404be822c702eb0c79f1929d7e4dd6df
emincanozcan <50255411+emincanozcan@users.noreply.github.com>
2021-12-30 16:20:59 +0300

    improve the installation experience

---
+++ datasource/helpers/seed.js
@@ -0,0 +1,128 @@
+import Clip from "../models/clip.js";
+import fetchYoutubeVideoInfo from "./fetchYoutubeVideoInfo.js";
+
+// TODO: this file should be refactored.
+
+function baseVideo(youtubeId, clips = []) {
+  return { youtubeId, clips };
+}
+
+function getBaseVideos() {
+  return {
+    vst2S09Ekjc: baseVideo("vst2S09Ekjc"),
+    A3JCB49SlSo: baseVideo("A3JCB49SlSo", [
+      { startsAt: 1, endsAt: 31 },
+      { startsAt: 37, endsAt: 53 },
+    ]),
+    DpJ8vsgok0o: baseVideo("DpJ8vsgok0o"),
+    wizgxRBfVTY: baseVideo("wizgxRBfVTY"),
+    r8E1Hq5tktg: baseVideo("r8E1Hq5tktg"),
+    v1xXlxDHWPU: baseVideo("v1xXlxDHWPU"),
+    dqv5i2zpoco: baseVideo("dqv5i2zpoco"),
+    ji888wNv1jw: baseVideo("ji888wNv1jw"),
+    "3fbhVPnhX-4": baseVideo("3fbhVPnhX-4"),
+    LLGeo_HPNn4: baseVideo("LLGeo_HPNn4"),
+    "3Ml-JTgFW1I": baseVideo("3Ml-JTgFW1I"),
+    lpZBwCp8rYc: baseVideo("lpZBwCp8rYc"),
+    M6T69LFIg1k: baseVideo("M6T69LFIg1k"),
+    uLJPdI4f6wE: baseVideo("uLJPdI4f6wE"),
+    oBo5ANETAdk: baseVideo("oBo5ANETAdk"),
+    UUtBsDIvBjM: baseVideo("UUtBsDIvBjM"),
+    bhl6y3K_rFc: baseVideo("bhl6y3K_rFc"),
+    "v_bnu-6oOwc": baseVideo("v_bnu-6oOwc"),
+    "CPY-Ou8MR44": baseVideo("CPY-Ou8MR44"),
+    "5fZU0DQYV34": baseVideo("5fZU0DQYV34"),
+    "3vofUNJo8hQ": baseVideo("3vofUNJo8hQ"),
+    cwB5R5FOOvU: baseVideo("cwB5R5FOOvU"),
+    SIUi8GDUlaE: baseVideo("SIUi8GDUlaE"),
+    "kjYpHcJU-q0": baseVideo("kjYpHcJU-q0"),
+    UVrrZZPAkvY: baseVideo("UVrrZZPAkvY"),
+
+    "6XvmhE1J9PY": baseVideo("6XvmhE1J9PY"),
+    "984VkHzXl8w": baseVideo("984VkHzXl8w"),
+    "cdeXlrq-tNw": baseVideo("cdeXlrq-tNw"),
+    Ym6whrAw8wU: baseVideo("Ym6whrAw8wU"),
+    x6fIseKzzH0: baseVideo("x6fIseKzzH0"),
+    "2_AMEAnWyRk": baseVideo("2_AMEAnWyRk"),
+    IyPr0VCiqsI: baseVideo("IyPr0VCiqsI"),
+    "PYJ22-YYNW8": baseVideo("PYJ22-YYNW8"),
+    "rbZ4aLq4-Lg": baseVideo("rbZ4aLq4-Lg"),
+    xEhjqUkYgx0: baseVideo("xEhjqUkYgx0"),
+    _mUvG6x53VM: baseVideo("_mUvG6x53VM"),
+    "1GlKdGZcP1E": baseVideo("1GlKdGZcP1E", [{ startsAt: 0, endsAt: 105 }]),
+    mOF54rIIw0Q: baseVideo("mOF54rIIw0Q", [{ startsAt: 8, endsAt: 177 }]),
+    I6vQtLdDbKI: baseVideo("I6vQtLdDbKI", [{ startsAt: 59, endsAt: 216 }]),
+    uZuwFAQpgZc: baseVideo("uZuwFAQpgZc", [{ startsAt: 0, endsAt: 134 }]),
+    beEPuUq_RdY: baseVideo("beEPuUq_RdY", [{ startsAt: 3, endsAt: 140 }]),
+    _rdINNHLYaQ: baseVideo("_rdINNHLYaQ", [
+      { startsAt: 0, endsAt: 163, title: "Machine Learning" },
+    ]),
+
+    "4Ix5g6Ictvs": baseVideo("4Ix5g6Ictvs", [
+      { startsAt: 0, endsAt: 92, title: "Great Advice for New Graudates" },
+    ]),
+
+    "1k89OTpDvIU": baseVideo("1k89OTpDvIU", [
+      { startsAt: 12, endsAt: 142, title: "Creating DIY Projects - Fawn Qiu" },
+    ]),
+    "0eD6WEpCSPg": baseVideo("0eD6WEpCSPg", [
+      { startsAt: 0, endsAt: 102, title: "True Richness" },
+    ]),
+    "FI-oPZnPRqI": baseVideo("FI-oPZnPRqI", [
+      { startsAt: 7, endsAt: 113, title: "Improve Your Concentration" },
+    ]),
+    iyEbvehRPhY: baseVideo("iyEbvehRPhY", [{ startsAt: 1, endsAt: 151 }]),
+    "8Nf9npmu3Xg": baseVideo("8Nf9npmu3Xg", [
+      { startsAt: 0, endsAt: 113, title: "Manage Your Career with Urgency" },
+    ]),
+  };
+}
+const seed = async () => {
+  const clipCount = await Clip.count();
+  if (clipCount > 0) {
+    return { message: "Already have records, delete them before seed." };
+  }
+
+  const seedCount = 6;
+
+  const allVideos = getBaseVideos();
+  const baseVideoData = {};
+  Object.keys(allVideos)
+    .sort(() => Math.round(Math.random()))
+    .slice(0, seedCount - 1)
+    .forEach((videoId) => {
+      baseVideoData[videoId] = allVideos[videoId];
+    });
+
+  const youtubeVideoInfo = await fetchYoutubeVideoInfo(
+    Object.keys(baseVideoData)
+  );
+
+  const clipData = [];
+
+  Object.values(baseVideoData).forEach((video) => {
+    const youtubeData = youtubeVideoInfo[video.youtubeId];
+    if (video.clips.length > 0) {
+      video.clips.forEach((clip) => {
+        clipData.push({
+          youtubeId: video.youtubeId,
+          youtubeVideoInfo: youtubeData,
+          title: youtubeData.title,
+          startsAt: clip.startsAt,
+          endsAt: clip.endsAt,
+        });
+      });
+    } else {
+      clipData.push({
+        youtubeId: video.youtubeId,
+        youtubeVideoInfo: youtubeData,
+        title: youtubeData.title,
+        startsAt: 0,
+        endsAt: youtubeData.duration,
+      });
+    }
+  });
+  return await Clip.create(clipData);
+};
+
+export default seed;
--- datasource/index.js
+++ datasource/index.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import express from "express";
 import Clip from "./models/clip.js";
-import fetchYoutubeVideoInfo from "./helpers/fetchYoutubeVideoInfo.js";
+import seed from "./helpers/seed.js";
 
 const app = express();
 const mongoUrl =
@@ -9,14 +9,18 @@ const mongoUrl =
 
 mongoose
   .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => {
+  .then(async (result) => {
+    if (process.env.SEED && parseInt(process.env.SEED) === 1) {
+      console.log("Seeding...");
+      await seed();
+      console.log("Seeding is completed.");
+    }
+
     app.listen(3000);
   })
   .catch((err) => {
     throw err;
   });
-
-// connect to mongodb
 
 app.get("/", async (req, res) => {
   return res.json({ status: "ok" });
@@ -39,112 +43,9 @@ app.get("/clips/:clipId", async (req, res) => {
   return res.json({ clip });
 });
 
-function baseVideo(youtubeId, clips = []) {
-  return { youtubeId, clips };
-}
-function getBaseVideos() {
-  return {
-    vst2S09Ekjc: baseVideo("vst2S09Ekjc"),
-    A3JCB49SlSo: baseVideo("A3JCB49SlSo", [
-      { startsAt: 1, endsAt: 31 },
-      { startsAt: 37, endsAt: 53 },
-    ]),
-    DpJ8vsgok0o: baseVideo("DpJ8vsgok0o"),
-    wizgxRBfVTY: baseVideo("wizgxRBfVTY"),
-    r8E1Hq5tktg: baseVideo("r8E1Hq5tktg"),
-    v1xXlxDHWPU: baseVideo("v1xXlxDHWPU"),
-    dqv5i2zpoco: baseVideo("dqv5i2zpoco"),
-    ji888wNv1jw: baseVideo("ji888wNv1jw"),
-    "3fbhVPnhX-4": baseVideo("3fbhVPnhX-4"),
-    LLGeo_HPNn4: baseVideo("LLGeo_HPNn4"),
-    "3Ml-JTgFW1I": baseVideo("3Ml-JTgFW1I"),
-    lpZBwCp8rYc: baseVideo("lpZBwCp8rYc"),
-    M6T69LFIg1k: baseVideo("M6T69LFIg1k"),
-    uLJPdI4f6wE: baseVideo("uLJPdI4f6wE"),
-    oBo5ANETAdk: baseVideo("oBo5ANETAdk"),
-    UUtBsDIvBjM: baseVideo("UUtBsDIvBjM"),
-    bhl6y3K_rFc: baseVideo("bhl6y3K_rFc"),
-    "v_bnu-6oOwc": baseVideo("v_bnu-6oOwc"),
-    "CPY-Ou8MR44": baseVideo("CPY-Ou8MR44"),
-    "5fZU0DQYV34": baseVideo("5fZU0DQYV34"),
-    "3vofUNJo8hQ": baseVideo("3vofUNJo8hQ"),
-    cwB5R5FOOvU: baseVideo("cwB5R5FOOvU"),
-    SIUi8GDUlaE: baseVideo("SIUi8GDUlaE"),
-    "kjYpHcJU-q0": baseVideo("kjYpHcJU-q0"),
-    UVrrZZPAkvY: baseVideo("UVrrZZPAkvY"),
-
-    "6XvmhE1J9PY": baseVideo("6XvmhE1J9PY"),
-    "984VkHzXl8w": baseVideo("984VkHzXl8w"),
-    "cdeXlrq-tNw": baseVideo("cdeXlrq-tNw"),
-    Ym6whrAw8wU: baseVideo("Ym6whrAw8wU"),
-    x6fIseKzzH0: baseVideo("x6fIseKzzH0"),
-    "2_AMEAnWyRk": baseVideo("2_AMEAnWyRk"),
-    IyPr0VCiqsI: baseVideo("IyPr0VCiqsI"),
-    "PYJ22-YYNW8": baseVideo("PYJ22-YYNW8"),
-    "rbZ4aLq4-Lg": baseVideo("rbZ4aLq4-Lg"),
-    xEhjqUkYgx0: baseVideo("xEhjqUkYgx0"),
-    _mUvG6x53VM: baseVideo("_mUvG6x53VM"),
-    "1GlKdGZcP1E": baseVideo("1GlKdGZcP1E", [{ startsAt: 0, endsAt: 105 }]),
-    mOF54rIIw0Q: baseVideo("mOF54rIIw0Q", [{ startsAt: 8, endsAt: 177 }]),
-    I6vQtLdDbKI: baseVideo("I6vQtLdDbKI", [{ startsAt: 59, endsAt: 216 }]),
-    uZuwFAQpgZc: baseVideo("uZuwFAQpgZc", [{ startsAt: 0, endsAt: 134 }]),
-    beEPuUq_RdY: baseVideo("beEPuUq_RdY", [{ startsAt: 3, endsAt: 140 }]),
-    _rdINNHLYaQ: baseVideo("_rdINNHLYaQ", [
-      { startsAt: 0, endsAt: 163, title: "Machine Learning" },
-    ]),
-
-    "4Ix5g6Ictvs": baseVideo("4Ix5g6Ictvs", [
-      { startsAt: 0, endsAt: 92, title: "Great Advice for New Graudates" },
-    ]),
-
-    "1k89OTpDvIU": baseVideo("1k89OTpDvIU", [
-      { startsAt: 12, endsAt: 142, title: "Creating DIY Projects - Fawn Qiu" },
-    ]),
-    "0eD6WEpCSPg": baseVideo("0eD6WEpCSPg", [
-      { startsAt: 0, endsAt: 102, title: "True Richness" },
-    ]),
-    "FI-oPZnPRqI": baseVideo("FI-oPZnPRqI", [
-      { startsAt: 7, endsAt: 113, title: "Improve Your Concentration" },
-    ]),
-    iyEbvehRPhY: baseVideo("iyEbvehRPhY", [{ startsAt: 1, endsAt: 151 }]),
-    "8Nf9npmu3Xg": baseVideo("8Nf9npmu3Xg", [
-      { startsAt: 0, endsAt: 113, title: "Manage Your Career with Urgency" },
-    ]),
-  };
-}
-
-app.get("/generate", async (req, res) => {
+app.get("/seed", async (req, res) => {
   await Clip.deleteMany();
-
-  const baseVideoData = getBaseVideos();
-  const youtubeVideoInfo = await fetchYoutubeVideoInfo(
-    Object.keys(baseVideoData)
-  );
-
-  const clipData = [];
-
-  Object.values(baseVideoData).forEach((video) => {
-    const youtubeData = youtubeVideoInfo[video.youtubeId];
-    if (video.clips.length > 0) {
-      video.clips.forEach((clip) => {
-        clipData.push({
-          youtubeId: video.youtubeId,
-          youtubeVideoInfo: youtubeData,
-          title: youtubeData.title,
-          startsAt: clip.startsAt,
-          endsAt: clip.endsAt,
-        });
-      });
-    } else {
-      clipData.push({
-        youtubeId: video.youtubeId,
-        youtubeVideoInfo: youtubeData,
-        title: youtubeData.title,
-        startsAt: 0,
-        endsAt: youtubeData.duration,
-      });
-    }
-  });
+  const clipData = await seed();
   try {
     const save = await Clip.create(clipData);
     return res.json({ save });
